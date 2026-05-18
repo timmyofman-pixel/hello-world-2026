40bb0c800ad9c45aa9fe7eae2ceaf8aed5d96a65
Christopher Timmerman <timmyofman@gmail.com>
2026-05-18 06:04:56 -0700

    added defer and getDocument

---
+++ ChristopherTimmerman/script.js
@@ -0,0 +1,3 @@
+const quote = document.getElementById("quote");
+
+quote.innerText = `"When my friends need some code who do they call? I can make an HTML for them all, even make a homepage for my dog." – Weird Al (White & Nerdy parody line)`;
